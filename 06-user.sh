3a7e07ec596ae961503d898245df3d428ec606bb
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-16 15:27:27 +0530

    roboshop

--- 06-user.sh
+++ 06-user.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
-
-app_name=catalogue
+app_name=user
 source ./common.sh
 check_root
-
 app_setup
 nodejs_setup
 systemd_setup
-
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Added Mongo repo" 
-
-dnf install mongodb-mongosh -y &>>$LOGS_FILE
-VALIDATE $? "Installed MongoDB client"
-
-INDEX=$(mongosh --host mongodb.lohithadev.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-
-if [ $INDEX -lt 0 ]; then
-    mongosh --host mongodb.lohithadev.online </app/db/master-data.js &>>$LOGS_FILE
-    VALIDATE $? "Load Products"
-else
-    echo -e "Products already loaded ... $Y SKIPPING $N"
-fi
-
 restart_service
 print_total_time
