b4ff1b7c53b9b70b9cfaf82ca3e0f9517746a6c0
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-16 14:34:43 +0530

    roboshop

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -5,14 +5,14 @@ check_root
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
 
-dnf install rabbitmq-server -y 
+dnf install rabbitmq-server -y &>> $LOG_FILE 
 VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server &>> $LOGS_FILE
-systemctl start rabbitmq-server &>> $LOGS_FILE
+systemctl enable rabbitmq-server &>> $LOG_FILE
+systemctl start rabbitmq-server &>> $LOG_FILE
 VALIDATE $? "Enabling and starting rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
+rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOG_FILE
 VALIDATE $? "setting up username and password"
 print_total_time
