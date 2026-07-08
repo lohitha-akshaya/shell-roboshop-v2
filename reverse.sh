394ef31ffa8a61e4244654ed2c7c2f5e8bd79d32
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-07-08 22:48:57 +0530

    ansible

--- reverse.sh
+++ reverse.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-max=$1
-for i in "$@"
+echo "enter a number:"
+read n
+flag=1
+i=2
+while [ $i <= n/2 ]
 do 
-   if [ $i -gt $max ]; then
-        max=$i
+   if [ n % i== 0]; then
+      flag=0
+      break
    fi
-done
-echo "maximum number is : $max"
+done 
+if [ $flag -eq 1 ]; then
+   echo "$n is a prime number"
+else
+   echo "$n is not a prime number"
+fi
