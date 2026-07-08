17b44cc61fe5ea1f4286aa0fc054a4db86f3915f
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-07-08 22:06:23 +0530

    ansible

--- reverse.sh
+++ reverse.sh
@@ -4,8 +4,8 @@ read n
 rno=0
 while [ "$n" -ne 0 ]
 do 
-    d=$($n%10)
-    rno=$($rno*10+$d)
-    n=$($n/10)
+    d=$(( $n % 10 ))
+    rno=$(( $rno * 10 + $d ))
+    n=$(( $n / 10 ))
 done
 echo "reverse number is : $rno"
